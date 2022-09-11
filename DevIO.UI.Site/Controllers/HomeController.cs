1bcbf7328946fdf280681f77b3f099f3eaaaa686
Gabriel <gabrielvmarra1@hotmail.com>
2022-09-11 16:45:42 -0300

    Initial commit

---
+++ DevIO.UI.Site/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using DevIO.UI.Site.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.UI.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IPedidoRepository _pedidoRepository { get; set; }
+
+        public HomeController(IPedidoRepository pedidoRepository)
+        {
+            _pedidoRepository = pedidoRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var pedido = _pedidoRepository.ObterPedido();
+
+            return View();
+        }
+
+        // Outra forma de injetar dependência
+        //public IActionResult Index([FromServices] IPedidoRepository _pedidoRepository)
+        //{
+        //    var pedido = _pedidoRepository.ObterPedido();
+
+        //    return View();
+        //}
+
+    }
+}
---
+++ DevIO.UI.Site/Controllers/TestCRUDController.cs
@@ -0,0 +1,42 @@
+﻿using DevIO.UI.Site.Data;
+using DevIO.UI.Site.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.UI.Site.Controllers
+{
+    public class TestCRUDController : Controller
+    {
+        private readonly MeuDbContext _contexto;
+
+        public TestCRUDController(MeuDbContext context)
+        {
+            _contexto = context;
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno()
+            {
+                Nome = "Gabriel",
+                DataNascimento = DateTime.Now,
+                Email = "gabriel@email.com"
+            };
+
+            _contexto.Alunos.Add(aluno);
+            _contexto.SaveChanges();
+
+            var alunoTabela = _contexto.Alunos.Find(aluno.Id);
+            var alunoTabela2 = _contexto.Alunos.FirstOrDefault(at => at.Email == aluno.Email);
+            var alunoTabela3 = _contexto.Alunos.Where(at => at.Nome == "Gabriel");
+
+            aluno.Nome = "João";
+
+            _contexto.Alunos.Update(aluno);
+            _contexto.SaveChanges();
+
+            _contexto.Alunos.Remove(aluno);
+            _contexto.SaveChanges();
+            return View();
+        }
+    }
+}
